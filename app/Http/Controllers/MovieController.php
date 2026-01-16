8cdd9ca989bf2cd5ce1c85d87652179ba18aca5a
nemiko007 <otoha.youjopero2@gmail.com>
2026-01-17 02:00:42 +0900

    aaaa

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('movies.index', ['movies' => $movies]);
+    }
+
+    public function admin()
+    {
+        $movies = Movie::all();
+        return view('admin.movies.index', ['movies' => $movies]);
+    }
+}
---
+++ database/migrations/2026_01_17_020000_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->text('title')->comment('映画タイトル');
+            $table->text('image_url')->comment('画像URL');
+            $table->integer('published_year')->comment('公開年');
+            $table->boolean('is_showing')->default(false)->comment('上映中かどうか');
+            $table->text('description')->comment('概要');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
